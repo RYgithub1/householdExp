e36c6f34f5243ac3ee3cf1b52a8ac8835ffae155
RYgithub1 <ryo.rec.otani@gmail.com>
2020-08-14 23:50:17 +0900

    implement_test_code

--- lib/householdExp/cli.rb
+++ lib/householdExp/cli.rb
@@ -14,7 +14,7 @@ module HouseholdExp
     desc "zaim mail password yyyymm", "expenses of a month(YYYYMM)"
     # メソッド定義
     def zaim(mailZaim, passwordZaim, monthZaim)
-      # インスタンス生成（ヘッドレス）
+      # インスタンス生成（ヘッドレスブラウザ）
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--headless')
       driver = Selenium::WebDriver.for :chrome, options: options
@@ -23,60 +23,58 @@ module HouseholdExp
       urlAuth = "https://auth.zaim.net/"
       driver.get urlAuth
 
-      # ログイン認証
+      # ログイン
       driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailZaim
       driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordZaim
       driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
 
-      # 履歴ページへ遷移（yyyymm）
-      urlHistory = "https://zaim.net/money?month=#{monthZaim}"
-      driver.get urlHistory
+      # ログインチェック
+      sleep 1
+      urlLogin = "https://zaim.net/home"
+      currentUrl = driver.current_url
+      if currentUrl == urlLogin then
+        # 《ログイン成功》
 
-      # メール/パスワード正否
-      wait = Selenium::WebDriver::Wait.new(timeout: 1)
-      begin
-        wait.until { driver.find_element(:class, 'HistorySearch-module__bodyArea___3AbtF') }
-      rescue
-        puts "\nLogin error: email ,password ,or yyyymm.\n\n"
-        driver.quit
-        return
-      end
-
-      # データ有無->取得->表示
-      puts "\n\n"
-      noLogCheck = "この月には、まだ記録がありませんでした。上部にある左右の矢印から前後の月に移動してください。"
-      if noLogCheck == driver.find_element(:class, 'HistorySearch-module__bodyArea___3AbtF').text then
-        puts "    ∩  ∩　＜ ご指定月(#{monthZaim})では記録なしです。）"
-        puts "｡ﾟ(ﾟ´Д｀ﾟ)ﾟ｡—————————————————————————————————\n\n\n"
-        puts "  ∩  ∩ 　＜ ご記入のほどお願いいたします！ ）"
-        puts "(*´ー｀)ﾉ———————————————————————————————————\n\n"
-      else
-        puts "  ∩ ∩ 　＜ ご指定月(#{monthZaim})の収支サマリです♪ ）"
-        puts "（・×・）————————————————————————————————————"
-        puts driver.find_element(:tag_name, 'table').text
-        puts "\n\n  ∩ ∩ 　＜ 明細はこちらです♫ ）"
-        puts "（・-・）————————————————————————————————————\n"
-        numberOfLines = driver.find_elements(:class, 'SearchResult-module__body___1CNGh').count
-        row = 1
-        while row <= numberOfLines do
-          puts "【日付 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[3]/span").text
-          puts "【ｶﾃｺﾞﾘ】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[4]/span[2]").text
-          puts "【金額 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[5]/span").text
-          puts "【出金 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[6]").text
-          puts "【入金 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[7]").text
-          puts "【お店 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[8]/span").text
-          puts "【品目 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[9]/span").text
-          puts "【メモ 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[10]/span").text
-          puts "\n-------------------------------------"
-          row += 1
+        # データ有無->取得->表示
+        puts "\n\n"
+        noLogCheck = "この月には、まだ記録がありませんでした。上部にある左右の矢印から前後の月に移動してください。"
+        if noLogCheck == driver.find_element(:class, 'HistorySearch-module__bodyArea___3AbtF').text then
+          puts "    ∩  ∩ ＜ ご指定月(#{monthZaim})では記録なしです。）"
+          puts "｡ﾟ(ﾟ´Д｀ﾟ)ﾟ｡—————————————————————————————————\n\n\n"
+          puts "  ∩  ∩ ＜ ご記入のほどお願いいたします！ ）"
+          puts "(*´ー｀)ﾉ———————————————————————————————————\n\n"
+        else
+          puts "  ∩ ∩ ＜ ご指定月(#{monthZaim})の収支サマリです♪ ）"
+          puts "（・×・）————————————————————————————————————"
+          puts driver.find_element(:tag_name, 'table').text
+          puts "\n\n  ∩ ∩ ＜ 明細はこちらです♫ ）"
+          puts "（・-・）————————————————————————————————————\n"
+          numberOfLines = driver.find_elements(:class, 'SearchResult-module__body___1CNGh').count
+          row = 1
+          while row <= numberOfLines do
+            puts "【日付 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[3]/span").text
+            puts "【ｶﾃｺﾞﾘ】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[4]/span[2]").text
+            puts "【金額 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[5]/span").text
+            puts "【出金 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[6]").text
+            puts "【入金 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[7]").text
+            puts "【お店 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[8]/span").text
+            puts "【品目 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[9]/span").text
+            puts "【メモ 】" + driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div/div[#{row}]/div[10]/span").text
+            puts "\n-------------------------------------"
+            row += 1
+          end
+          puts "\n\n  ∩ ∩ ＜ ご確認頂きありがとうございます！ ）"
+          puts "（・▽・）————————————————————————————————————\n\n"
         end
-        puts "\n\n  ∩ ∩ 　＜ ご確認頂きありがとうございます！ ）"
-        puts "（・▽・）————————————————————————————————————\n\n"
+        # ブラウザ終了
+        driver.quit
+
+      else
+        # 《ログイン失敗》
+        puts "\nLogin error: mail or password.\n\n"
+        # ブラウザ終了
+        driver.quit
       end
-
-      # ブラウザ終了
-      driver.quit
     end
-
   end
 end
--- spec/householdExp_spec.rb
+++ spec/householdExp_spec.rb
@@ -1,7 +1,120 @@
+require "selenium-webdriver"
+
+
 RSpec.describe HouseholdExp do
+  describe "RSpec_Application_Test" do
 
-  it "has a version number" do
-    expect(HouseholdExp::VERSION).not_to be nil
+    describe "#version" do
+      it "has a version number" do
+        expect(HouseholdExp::VERSION).not_to be nil
+      end
+    end
+
+    describe "#cli_ログイン" do
+      context "ログイン可能な場合" do
+        it "【right】mail/password <-> 【wrong】" do
+          mailZaim = "householdExpenses316@gmail.com"
+          passwordZaim = "expense3"
+          driver = Selenium::WebDriver.for :chrome
+          urlAuth = "https://auth.zaim.net/"
+          driver.get urlAuth
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
+          sleep 1
+          urlLogin = "https://zaim.net/home"
+          expect(driver.current_url).to eq urlLogin
+          driver.quit
+        end
+      end
+      context "ログイン不可な場合" do
+        it "【right】mail <-> 【wrong】password" do
+          mailZaim = "householdExpenses316@gmail.com"
+          passwordWrong = "zzzzzzz3"
+          driver = Selenium::WebDriver.for :chrome
+          urlAuth = "https://auth.zaim.net/"
+          driver.get urlAuth
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordWrong
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
+          sleep 1
+          expect(driver.current_url).to eq urlAuth
+          driver.quit
+        end
+        it "【right】password <-> 【wrong】mail" do
+          mailWrong = "zzzzzzzzzzzzzzzzzzzz@zzzzz.com"
+          passwordZaim = "expense3"
+          driver = Selenium::WebDriver.for :chrome
+          urlAuth = "https://auth.zaim.net/"
+          driver.get urlAuth
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailWrong
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
+          sleep 1
+          expect(driver.current_url).to eq urlAuth
+          driver.quit
+        end
+        it "【right】 <-> 【wrong】mail/password" do
+          mailWrong = "zzzzzzzzzzzzzzzzzzzz@zzzzz.com"
+          passwordWrong = "zzzzzzz3"
+          driver = Selenium::WebDriver.for :chrome
+          urlAuth = "https://auth.zaim.net/"
+          driver.get urlAuth
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailWrong
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordWrong
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
+          sleep 1
+          expect(driver.current_url).to eq urlAuth
+          driver.quit
+        end
+      end
+    end
+
+    describe "#cli_履歴表示" do
+      context "入力月が有効な場合" do
+        it "【right】mail/password/month(記録有り：202008) <-> 【wrong】" do
+          mailZaim = "householdExpenses316@gmail.com"
+          passwordZaim = "expense3"
+          monthZaim = "202008"
+          isLogText = "金融連携が入出金先に指定されている入力を追加・編集すると残高が狂うことがあります。サポート対象外となりますので、ご自分で残高の調整をお願いいたします。 メモなどが見えない場合は、表を横にスクロールしてください。"
+          driver = Selenium::WebDriver.for :chrome
+          urlAuth = "https://auth.zaim.net/"
+          driver.get urlAuth
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
+          urlHistory = "https://zaim.net/money?month=#{monthZaim}"
+          driver.get urlHistory
+          sleep 2
+          expect(driver.find_element(:xpath, "/html/body/div[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]").text).to eq isLogText
+          driver.quit
+        end
+        it "【right】mail/password/month(記録無し：202007) <-> 【wrong】" do
+          mailZaim = "householdExpenses316@gmail.com"
+          passwordZaim = "expense3"
+          monthZaim = "202007"
+          noLogText = "この月には、まだ記録がありませんでした。上部にある左右の矢印から前後の月に移動してください。"
+          driver = Selenium::WebDriver.for :chrome
+          urlAuth = "https://auth.zaim.net/"
+          driver.get urlAuth
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[2]/div/input').send_keys mailZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[3]/div/input').send_keys passwordZaim
+          driver.find_element(:xpath, '/html/body/div[3]/div[2]/div[1]/div[2]/form/div[4]/input').click
+          urlHistory = "https://zaim.net/money?month=#{monthZaim}"
+          driver.get urlHistory
+          sleep 2
+          expect(driver.find_element(:class, 'HistorySearch-module__bodyArea___3AbtF').text).to eq noLogText
+          driver.quit
+        end
+      end
+      context "入力月が無効な場合" do
+        it "【right】mail/password <-> 【wrong】month(桁：123456789)" do
+    
+        end
+        it "【right】mail/password <-> 【wrong】month(年月：777777)" do
+        
+        end
+      end
+    end
   end
-
 end
